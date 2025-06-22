bf742d8041f9160cfa6ed11832e72ff1e3df7efa
Jakoby1835 <Jakoby1835@gmail.com>
2025-06-22 12:53:29 +0200

    added comment and copy of 5.6

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,6 +3,8 @@
 
 #include <catch.hpp>
 #include <numbers>
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
 
 #include "Sphere.hpp"
 #include "Box.hpp"
@@ -110,7 +112,42 @@ TEST_CASE("PrintShapes", "[Shape][Box][Sphere]") {
         std::cout << sphere1 << std::endl;
         std::cout << sphere2 << std::endl;
     }
+    /*    Virtuelle Funktionen:
+    virtual in der Basis - Klasse ermöglicht Überschreibung
+        = 0 macht die Funktion rein virtuell
+        Ermöglicht polymorphes Verhalten bei Laufzeit
+        Override - Schlüsselwort:
+    Garantiert korrekte Überschreibung der virtuellen Funktion
+        Verhindert Fehler bei der Signatur
+        Macht die Überschreibung explizit im Code sichtbar
+        Stream - Operator :
+        Ermöglicht einfache Ausgabe mit <<
+        Nutzt die virtuelle print() - Methode
+        Funktioniert mit allen abgeleiteten Klassen
+        Vererbungshierarchie :
+    Shape als abstrakte Basis - Klasse
+        Box und Sphere als konkrete abgeleitete Klassen
+        Jede abgeleitete Klasse implementiert ihre eigene print() - Methode*/
+}
 
+TEST_CASE("intersect_ray_sphere", "[intersect]")
+{
+    // Ray
+    glm::vec3 ray_origin{ 0.0f, 0.0f, 0.0f };
+    // ray direction has to be normalized !
+    // you can use:
+    // v = glm::normalize(some_vector)
+    glm::vec3 ray_direction{ 0.0f, 0.0f, 1.0f };
+    // Sphere
+    glm::vec3 sphere_center{ 0.0f ,0.0f, 5.0f };
+    float sphere_radius{ 1.0f };
+    float distance = 0.0f;
+    auto result = glm::intersectRaySphere(
+        ray_origin, ray_direction,
+        sphere_center,
+        sphere_radius * sphere_radius, // squared radius !!!
+        distance);
+    REQUIRE(distance == Approx(4.0f));
 }
 
 int main(int argc, char *argv[])
